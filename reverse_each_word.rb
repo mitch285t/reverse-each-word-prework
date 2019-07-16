3147d330eb88ab4206441661d972eca6919f709b
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-16 16:54:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,4 +9,9 @@ end
 newarray.join
 end
 
-  
+  def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
