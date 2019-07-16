0ab8e84e46386b02477f036440a9216887517f3f
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-16 16:55:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,4 @@
-
-
-def reverse_each_word(string)
-string.split("")
-newarray = []
-newarray = string.each do |string|
-  string.reverse 
-end
-newarray.join
-end
-
-  def reverse_each_word(sentence)
+ def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
